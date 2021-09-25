55a25d842c22428ac4319d184db8ce1ca8256c40
yankvasya <yankvasya@gmail.com>
2021-09-25 05:24:45 +0300

    session placemarks + localstorage

--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ require('./index.html');
 
 ymaps.ready(init);
 
+let currentCoordinates = [];
+const balloon = `<form class="form" id="form" action="">
+                    <label><h2 class="map__feedback">Отзыв:</h2></label>
+                    <label><input class="form__name" name="name" type="text" placeholder="Укажите ваше имя"></label>
+                    <label><input class="form__location" name="location" type="text" placeholder="Укажите место"></label>
+                    <label><textarea class="form__review" name="review" id="review" cols="40" rows="4" placeholder="Оставьте отзыв"></textarea></label>
+                    <label><button id="submit" type="submit">Добавить</button></label>
+                    </form>`;
+let placemarks = takeAllPlacemarks();
+let geoObjects = [];
+let newGeoObjects = [];
+
+
 function init() {
     const map = new ymaps.Map('map', {
         center: [55.75, 37.64],
@@ -11,83 +24,138 @@ function init() {
         ],
         behaviors: ['drag']
     });
-
-    const balloon = `<div class="map__balloon">дом 63 строение 2</div>
-                    <div class="map__feedback">Отзыв:</div>
-                    <form class="form" id="form" action="">
-                    <label><input class="form__name" name="name" type="text" placeholder="Укажите ваше имя"></label>
-                    <label><input class="form__location" name="location" type="text" placeholder="Укажите место"></label>
-                    <label><textarea class="form__review" name="review" id="review" cols="40" rows="4" placeholder="Оставьте отзыв"></textarea></label>
-                    <label><button id="submit" type="submit">Добавить</button></label>
-                    </form>`;
-
-    const placemarks = [
-        {
-            latitude: 55.72,
-            longitude: 37.64,
-            hintContent: '<div class="map__hint">Техносад</div>',
-            balloonContent: balloon
-        },
-        {
-            latitude: 55.73,
-            longitude: 37.66,
-            hintContent: '<div class="map__hint">Вторая точка</div>',
-            balloonContent: balloon
-        },
-        {
-            latitude: 55.70,
-            longitude: 37.62,
-            hintContent: '<div class="map__hint">Третья точка</div>',
-            balloonContent: balloon
-        },
-        {
-            latitude: 55.75,
-            longitude: 37.62,
-            hintContent: '<div class="map__hint">Четвертая точка</div>',
-            balloonContent: balloon
-        }
-    ], geoObjects = [];
-
-    for (const mark of placemarks) {
-        const { latitude, longitude, hintContent, balloonContent } = mark;
-        geoObjects.push(new ymaps.Placemark([latitude, longitude],
-            {
-                hintContent: `<div class="map__hint">${hintContent}</div>`,
-                balloonContent: balloonContent
-            },
-            {
-                iconLayout: 'default#image',
-                iconImageHref: 'https://cdn-icons-png.flaticon.com/512/506/506403.png',
-                iconImageSize: [30, 30],
-                iconImageOffset: [-15, -30]
-            }
-        ));
-    }
-
     const cluster = new ymaps.Clusterer({
         clusterIcons: [
             {
-                href: 'https://cdn-icons-png.flaticon.com/512/149/149724.png',
+                href: 'https://static.thenounproject.com/png/331066-200.png',
                 size: [70, 70],
                 offset: [-35, -35]
             }
         ],
-        clusterIconContentLayout: null
     });
+
+    setInterval(checkNewPlacemarks, 500)
+
+
+    for (const mark of placemarks) {
+       const { latitude, longitude, hintContent, balloonContent } = mark;
+       geoObjects.push(new ymaps.Placemark([latitude, longitude],
+           {
+               hintContent: `<div class="map__hint">${hintContent}</div>`,
+               balloonContent: balloonContent
+           },
+           {
+               iconLayout: 'default#image',
+               iconImageHref: 'https://cdn-icons-png.flaticon.com/512/506/506403.png',
+               iconImageSize: [30, 30],
+               iconImageOffset: [-15, -30]
+           }
+       ));
+    }
 
     map.geoObjects.add(cluster);
     cluster.add(geoObjects);
+
+    function checkNewPlacemarks() {
+        if(newGeoObjects.length) {
+            (function () {
+                map.geoObjects.add(cluster);
+                cluster.add(newGeoObjects);
+                newGeoObjects = [];
+                console.log('новый элемент добавлен');
+            })();
+        }
+    }
+
+    map.events.add('click', function (e) {
+        if (!map.balloon.isOpen()) {
+            var coords = e.get('coords');
+            map.balloon.open(coords, {
+                contentFooter: checkLocalStorage(coords),
+                contentBody: balloon
+                // contentHeader:'Событие!',
+            });
+        }
+        else {
+            map.balloon.close();
+        }
+    });
 }
 
+function checkLocalStorage([latitude, longitude]) {
+    const coords = `<div>Новая точка</div>
+                    <div>Ширина: ${latitude}</div>
+                    <div>Долгота: ${longitude}</div>`
+
+    currentCoordinates = [latitude, longitude];
+
+    return coords;
+}
+
+function takeAllPlacemarks() {
+    let allPlacemarks = [];
+
+    for (key in localStorage) {
+        let arrayPlacemark = {};
+        const coordsSplited = key.split(',');
+
+        if(Number(!isNaN(Number(coordsSplited[0])) && !isNaN(Number(coordsSplited[0])))) {
+            if(coordsSplited[0] !== undefined && coordsSplited[1] !== undefined) {
+                const newCoords = [Number(coordsSplited[0].substr(0,5)),
+                    Number(coordsSplited[1].substr(0,5))
+                ];
+
+                arrayPlacemark = {
+                    latitude: newCoords[0],
+                    longitude: newCoords[1],
+                    hintContent: `<div class="map__hint">Широта: ${newCoords[0]}; Долгота: ${newCoords[1]}</div>`,
+                    balloonContent: balloon
+                }
+
+                allPlacemarks.push(arrayPlacemark);
+            }
+        }
+    }
+
+    return allPlacemarks;
+}
+
+document.addEventListener('click', (e) => {
+      e.preventDefault();
+      addNewPlaceMark(e);
+  });
+
+function addNewPlaceMark(e) {
+    if(e.target.tagName === 'BUTTON') {
+        e.preventDefault();
+        const form = e.target.closest('form');
+        const name = form.elements.name.value;
+        const location = form.elements.location.value;
+        const review = form.elements.review.value;
+        const infoPlacemark = {
+            name: name,
+            location: location,
+            review: review
+        };
+        const stringInfo = JSON.stringify(infoPlacemark);
+        const close = document.querySelector('.ymaps-2-1-79-balloon__close-button');
+
+        newGeoObjects.push(new ymaps.Placemark([currentCoordinates[0], currentCoordinates[1]],
+                    {
+                        hintContent: `<div class="map__hint">Ширина: ${currentCoordinates[0]}; Долгота: ${currentCoordinates[1]}</div>`,
+                        balloonContent: balloon
+                    },
+                    {
+                        iconLayout: 'default#image',
+                        iconImageHref: 'https://cdn-icons-png.flaticon.com/512/506/506403.png',
+                        iconImageSize: [30, 30],
+                        iconImageOffset: [-15, -30]
+                    }
+                ));
 
 
-window.addEventListener('load', ()=> {
-    const formSubmit = document.querySelector('#form');
-    const submitButton = document.querySelector('#submit');
 
-    console.log('Страница загрузилась');
-
-    // formSubmit.addEventListener('click', (e) => {
-    //     console.log(e.target)
-    // })
-})
+        localStorage[currentCoordinates] = stringInfo;
+        close.dispatchEvent(new Event("click", {bubbles: true}));
+    }
+}
